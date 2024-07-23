5f4df6eb61beb7a90d1839a330d8c564144d342a
Mia Papazian <miacpapazian@gmail.com>
2024-07-23 13:29:45 -0400

    wrote in for loop to analyze four other species. already analysed candida_parapsilosis
    Former-commit-id: ce1f8a5a349ff9f79a0d513001a6f20f3b8f0f12
    Former-commit-id: 0a15e79346277cea9709ae88162c9a53b9a09917

--- runRoc_parallel.sh
+++ runRoc_parallel.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # Description: ROC Analysis
-# Usage: ./runRoc_parallel.sh <SPECIES> <INPUT_DIR> <OUTPUT_DIR>
+# Usage: ./runRoc_parallel.sh 
 
-SPECIES="candida_parapsilosis"
+#species list 
+SPECIES_LIST=("saccharomyces_uvarum" "ogataea_methanolica" "ogataea_parapolymorpha" "ogataea_polymorpha")
+
 INFOLDER="fasta/revisit_cds_data"
+OUTPUT_BASE="Results/ConstMut"
 SUFFIX=".max.cds"
 
+#loop through each species
+for SPECIES in "${SPECIES_LIST[@]}"; do 
+
 INPUT="${INFOLDER}/${SPECIES}${SUFFIX}"
-OUTPUT_BASE="Results/ConstMut"
 
 #results saved to a unique subfolder using the species name
 OUTPUT="${OUTPUT_BASE}/${SPECIES}"
 mkdir -p "$OUTPUT"
 
+
+echo "Running ROC analysis for ${SPECIES}..."
 nohup Rscript --vanilla Scripts/rocAnalysis.R -i "$INPUT" -o "$OUTPUT" -d 0 -s 1000 -a 20 -t 5 -n 8 --est_csp --est_phi --est_hyp --max_num_runs 2 --codon_table 1 &> "${OUTPUT}/roc_analysis.log" &
 wait
+done
