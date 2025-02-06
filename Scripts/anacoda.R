03ee35aefc1ea9ecf76700c1c9fde53caa1b7c15
Mia Papazian <miacpapazian@gmail.com>
2025-02-06 12:03:29 -0500

    still having trouble extracting traces. output not recording?

--- Scripts/anacoda.R
+++ Scripts/anacoda.R
@@ -3,7 +3,26 @@
 library(AnaCoDa)
 genome <- initializeGenomeObject(file = "fasta/revisit_cds_data/candida_tenuis.max.cds")
 parameter <- initializeParameterObject(genome = genome, sphi = 1, num.mixtures = 1, gene.assignment = rep(1, length(genome)))
-mcmc <- initializeMCMCObject(samples = 50, thinning = 10, adaptive.width=50)
+mcmc <- initializeMCMCObject(samples = 2000, thinning = 10, adaptive.width = 50)
 model <- initializeModelObject(parameter = parameter, model = "ROC")
-runMCMC(mcmc = mcmc, genome = genome, model = model)
 
+mcmc_results <- runMCMC(mcmc = mcmc, genome = genome, model = model)
+
+if (!is.null(mcmc_results)) {
+  save(mcmc_results, file = "mcmc_results.RData")
+  print("MCMC results saved successfully!")
+  print(mcmc_results)
+} else {
+  print("MCMC did not return results.")
+}
+
+save(mcmc_results, file = "mcmc_results.RData")
+
+print(mcmc_results)
+
+
+#checking the data 
+genome_names <- getNames(genome)
+print(genome_names)
+codon_counts <- getCodonCounts(genome)
+print(codon_counts)
