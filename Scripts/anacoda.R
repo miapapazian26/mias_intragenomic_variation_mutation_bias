ecc67d7dcf509e9f489ae937e852cbac9d9777a0
Mia Papazian <miacpapazian@gmail.com>
2025-02-10 16:23:40 -0500

    actually removed ribmodelframework from this repo bc nested git repo = bad. plotted trace for candida_tenuis.max.cds

--- Scripts/anacoda.R
+++ Scripts/anacoda.R
@@ -1,28 +1,33 @@
 #ex: using codon data in the form of CDS in fasta format with one mixture (ROC)
 #the following example illustrates how you would estimates parameters under the ROC model of a given set of protein coding genes, assuming the same mutation and selection regime for all genes.
 library(AnaCoDa)
+
+#initialize genome object
 genome <- initializeGenomeObject(file = "fasta/revisit_cds_data/candida_tenuis.max.cds")
+
+#initialize parameter object
 parameter <- initializeParameterObject(genome = genome, sphi = 1, num.mixtures = 1, gene.assignment = rep(1, length(genome)))
+
+#save parameter object
+param_file <- file.path(tempdir(), "parameter.Rda")
+writeParameterObject(parameter = parameter, file = param_file)
+
+#initialize MCMC object
 mcmc <- initializeMCMCObject(samples = 100, thinning = 10, adaptive.width = 50)
+
+#initialize model
 model <- initializeModelObject(parameter = parameter, model = "ROC")
 
-mcmc_results <- runMCMC(mcmc = mcmc, genome = genome, model = model)
+#run MCMC (modifies 'mcmc' in place)
+runMCMC(mcmc = mcmc, genome = genome, model = model)
 
-if (!is.null(mcmc_results)) {
-  save(mcmc_results, file = "mcmc_results.RData")
-  print("MCMC results saved successfully!")
-  print(mcmc_results)
-} else {
-  print("MCMC did not return results.")
-}
+#save updated MCMC object 
+mcmc_file <- file.path(tempdir(), "mcmc_results.Rda")
+writeMCMCObject(mcmc = mcmc, file = mcmc_file)
 
-save(mcmc_results, file = "mcmc_results.RData")
+load(file.path(tempdir(), "mcmc_results.Rda"))
+print(mcmc)
+plot(mcmc$getLogPosteriorTrace(), type = "l", main = "MCMC Log-Posterior Trace")
 
-print(mcmc_results)
-
-
-#checking the data 
-genome_names <- getNames(genome)
-print(genome_names)
-codon_counts <- getCodonCounts(genome)
-print(codon_counts)
+trace <- parameter$getTraceObject()
+plot(x = trace, what = "Expression", mixture = 1, geneIndex = 669)
