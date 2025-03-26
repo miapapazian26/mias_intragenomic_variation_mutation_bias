a1c62ff41300aa711f1d0d0849cb3485d3692902
Mia Papazian <miacpapazian@gmail.com>
2025-03-26 16:51:09 -0400

    fixing labelling for the llik function, changed delta_M to dM_trace and same with delta eta

--- Scripts/usage.R
+++ Scripts/usage.R
@@ -1,22 +1,22 @@
 #checking and usage 
-head(delta_M) #looking delta M **fix name
-head(delta_eta) #looking delta eta **fix name
+head(dM_trace) #looking delta M 
+head(dE_trace) #looking delta eta 
 head(codon_counts) #looking at codon counts 
 head(phi_trace) #looking at phi values 
 head(codon_names) #looking at codon names
 head(gene_index) #looking at gene indexes **can be dropped using seq_along(gene_names)
 head(gene_names) #looking at gene names
 
-str(delta_M) #looking delta M **fix name
-str(delta_eta) #looking delta eta **fix name
+str(dM_trace) #looking delta M 
+str(dE_trace) #looking delta eta 
 str(codon_counts) #looking at codon counts 
 str(phi_trace) #looking at phi values 
 str(codon_names) #looking at codon names
 str(gene_index) #looking at gene indexes
 str(gene_names) #looking at gene names 
 
-LLikGene(gene_name, delta_M, delta_eta, phi, codon_counts)
+LLikGene(gene_name, dM_trace, dE_trace, phi, codon_counts)
 
-LLikAA(delta_M, delta_eta, phi, codon_counts) 
+LLikAA(dM_trace, dE_trace, phi, codon_counts) 
 
 
